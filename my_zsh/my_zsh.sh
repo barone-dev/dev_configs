bac5c11834ebecf98a4d0c75bb20403f5c55dd5f
marco.barone <marco.barone@casacontrolada.com.br>
2019-07-09 10:33:30 -0300

    services

--- my_zsh/my_zsh.sh
+++ my_zsh/my_zsh.sh
@@ -2,3 +2,5 @@ source ~/dev_configs/my_zsh/aliases/aliases.sh
 
 # Add secret local files to bash:
 source ~/dev_configs/secret/all.sh
+
+source ~/dev_configs/my_zsh/services.sh
---
+++ my_zsh/services.sh
@@ -0,0 +1,10 @@
+export LANG=en_US.UTF-8
+
+# Oracle
+export LD_LIBRARY_PATH=/usr/lib/oracle/12.1/lib
+
+
+# Golang
+export GOROOT=/usr/local/go
+# export GOPATH=$HOME/Projects/Proj1
+export PATH=$GOPATH/bin:$GOROOT/bin:$PATH
