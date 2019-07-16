db568cfddbdaf6001806dc3a8039bb5e6074f26b
marco.barone <marco.barone@casacontrolada.com.br>
2019-07-16 11:30:38 -0300

    pip_no_cache added to aliases

--- my_zsh/aliases/aliases.sh
+++ my_zsh/aliases/aliases.sh
@@ -14,6 +14,7 @@ alias python="python3"
 alias pip="pip3"
 alias new_venv="python3 -m venv venv"
 alias venv=". venv/bin/activate"
+alias pip_no_cache="pip install -U --no-cache-dir -r ${}"
 
 # Git shortcuts:
 alias st="git status"
